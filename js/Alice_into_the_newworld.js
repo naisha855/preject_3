f8380633d8b49e7dd5f2f8c59ef8b63f3daa1dac
naisha855 <156628341+naisha855@users.noreply.github.com>
2024-03-04 17:40:28 +0900

    2024-03-04

--- js/Alice_into_the_newworld.js
+++ js/Alice_into_the_newworld.js
@@ -13,3 +13,27 @@ $('.').on("mousewheel", function(e){
         return false;
     }
 })
+
+$(function(){
+    $('.section_3_chapterpage_buttonpage_next').click(function(){
+        let idx = $(this).index();
+
+        $(this).addClass('');
+        $(this).siblings().removeClass('');
+
+        $('.').eq(idx).fadeIn().css('display', 'flex');
+        $('.').eq(idx).siblings('.').hide();
+    })
+})
+
+// $(function () {
+//     $('.best_swichi_button').click(function () {
+//        let idx = $(this).index();
+
+//        $(this).addClass('one');
+//        $(this).siblings().removeClass('one');
+
+//        $('.best_list').eq(idx).fadeIn().css('display', 'flex');
+//        $('.best_list').eq(idx).siblings('.best_list').hide();
+//    });
+// });

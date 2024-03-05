375fcced873f826446e7fdbc4b8fa3c9b8992e2a
naisha855 <156628341+naisha855@users.noreply.github.com>
2024-03-05 17:40:26 +0900

    2024-03-05

--- js/Alice_into_the_newworld.js
+++ js/Alice_into_the_newworld.js
@@ -2,18 +2,36 @@ $(function(){
     var win_h = $(window).height();
 });
 
+// 휠 이동
+// $('.').on("mousewheel", function(e){
+//     var pagePos = parseInt($(this).attr());
+//     if(e.originalEvent.wheelDelta>=0){
+//         $("html, body").stop().animate({scrollTop:pagePos - win_h});
+//         return false;
+//     } else if(e.originalEvent.wheelDelta<0){
+//         $("html, body").stop().animate({scrollTop:pagePos+win_h});
+//         return false;
+//     }
+// })
 
-$('.').on("mousewheel", function(e){
-    var pagePos = parseInt($(this).attr());
-    if(e.originalEvent.wheelDelta>=0){
-        $("html, body").stop().animate({scrollTop:pagePos - win_h});
-        return false;
-    } else if(e.originalEvent.wheelDelta<0){
-        $("html, body").stop().animate({scrollTop:pagePos+win_h});
-        return false;
-    }
+// section_2 effect
+let w=$(window).innerWidth();
+
+console.log(w)
+$('.section_2_button_page_prev').click(function(){
+    $('.section_2_paper li:last').prependTo('.section_2_paper');
+    $('.section_2_paper').css('margin-left',-w);
+    $('.section_2_paper').stop().animate({marginLeft:0}, 800);
 })
 
+$('.section_2_button_page_next').click(function(){
+    $('.section_2_paper').stop().animate({marginLeft: -w}, 800, function(){
+        $('.section_2_paper li:first').appendTo('.section_2_paper');
+        $('.section_2_paper').css({marginLeft:0});
+    });
+});
+
+// Section_3 effect
 $(function(){
     $('.section_3_chapterpage_buttonpage_next').click(function(){
         let idx = $(this).index();

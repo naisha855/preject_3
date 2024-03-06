e801e75899846e90eb19a40c1a47caf0802b3956
naisha855 <156628341+naisha855@users.noreply.github.com>
2024-03-06 17:39:04 +0900

    2024-03-06

--- js/Alice_into_the_newworld.js
+++ js/Alice_into_the_newworld.js
@@ -1,48 +1,78 @@
-$(function(){
+$(function () {
     var win_h = $(window).height();
+    $('.page').each(function (index) {
+        $(this).attr("data-index", win_h * index);
+    });
+
+
+    // 휠 이동
+    $('.page').on("mousewheel", function (e) {
+        console.log($(this))
+        var pagePos = parseInt($(this).attr("data-index"));
+        if (e.originalEvent.wheelDelta >= 0) {
+            $("html, body").stop().animate({ scrollTop: pagePos - win_h });
+            return false;
+        } else if (e.originalEvent.wheelDelta < 0) {
+            $("html, body").stop().animate({ scrollTop: pagePos + win_h });
+            return false;
+        }
+    })
 });
-
-// 휠 이동
-// $('.').on("mousewheel", function(e){
-//     var pagePos = parseInt($(this).attr());
-//     if(e.originalEvent.wheelDelta>=0){
-//         $("html, body").stop().animate({scrollTop:pagePos - win_h});
-//         return false;
-//     } else if(e.originalEvent.wheelDelta<0){
-//         $("html, body").stop().animate({scrollTop:pagePos+win_h});
-//         return false;
-//     }
-// })
-
 // 타이핑
 
-// var typingBool = false; 
-// var typingIdx=0; 
+var typingBool = false; 
+var typingIdx=0; 
+var liIndex = 0;
+var liLength = $(".typing_txt>li").length;
 
-// // 타이핑될 텍스트를 가져온다 
-// var typingTxt = $(".typing-txt").text(); 
+// 타이핑될 텍스트를 가져온다 
+var typingTxt = $(".typing_txt>li").eq(liIndex).text(); 
 
-// typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
-
-// if(typingBool==false){ 
-//   // 타이핑이 진행되지 않았다면 
-//    typingBool=true;     
-//    var tyInt = setInterval(typing,100); // 반복동작 
-// } 
+typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+console.log(typingTxt)
+if(typingBool==false){ 
+  // 타이핑이 진행되지 않았다면 
+   typingBool=true;     
+   var tyInt = setInterval(typing,150); // 반복동작 
+} 
      
-// function typing(){ 
-//   if(typingIdx<typingTxt.length){ 
-//     // 타이핑될 텍스트 길이만큼 반복 
-//     $(".typing").append(typingTxt[typingIdx]);
-//     // 한글자씩 이어준다. 
-//     typingIdx++; 
-//    } else{ 
-//      //끝나면 반복종료 
-//     clearInterval(tyInt); 
-//    } 
-// }  
+function typing(){ 
+    $(".typing li").removeClass("on");
+    $(".typing li").eq(liIndex).addClass("on");
+  if(typingIdx<typingTxt.length){ 
+    // 타이핑될 텍스트 길이만큼 반복 
+    $(".typing li").eq(liIndex).append(typingTxt[typingIdx]);
+    // 한글자씩 이어준다. 
+
+    typingIdx++; 
+   } else if(liIndex<liLength-1){
+     //다음문장으로  가기위해 인덱스를 1증가
+       liIndex++; 
+     //다음문장을 타이핑하기위한 셋팅
+        typingIdx=0;
+        typingBool = false; 
+        typingTxt = $(".typing_txt>li").eq(liIndex).text(); 
+       
+     //다음문장 타이핑전 1초 쉰다
+         clearInterval(tyInt);
+          //타이핑종료
+     
+         setTimeout(function(){
+           //1초후에 다시 타이핑 반복 시작
+           tyInt = setInterval(typing,100);
+         },1000);
+        } else if(liIndex==liLength-1){
+          
+         //마지막 문장까지 써지면 반복종료
+           clearInterval(tyInt);
+        }
+    } 
+
+
+
 
 // section_2 effect
+
 let w=$(window).innerWidth();
 
 console.log(w)
@@ -69,8 +99,8 @@ $(function(){
 
         $('.').eq(idx).fadeIn().css('display', 'flex');
         $('.').eq(idx).siblings('.').hide();
-    })
-})
+    });
+});
 
 // $(function () {
 //     $('.best_swichi_button').click(function () {
